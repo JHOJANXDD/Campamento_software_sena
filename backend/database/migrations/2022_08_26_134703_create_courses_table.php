570c785a67077b11c4789b40442b1bdf3b59ca43
kvelasco158 <kyvelasco04@misena.edu.co>
2022-08-26 09:28:20 -0500

    migraciones

---
+++ backend/database/migrations/2022_08_26_134703_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 100);
+            $table->text('Description');
+            $table->integer('weeks', 10);
+            $table->string('enroll_cost', 100);
+            $table->string('minimu_skill', 100);
+            $table->foreignId('bootcamp_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
